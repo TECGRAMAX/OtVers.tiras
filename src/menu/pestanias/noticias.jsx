9bdc831c7e812cba36fcad18a66d904351908ed4
TECGRAMAX <donmaximilianoiiidelaplata@gmail.com>
2026-03-04 03:16:08 +0100

    fix: move useState hook outside array in src

--- src/menu/pestanias/noticias.jsx
+++ src/menu/pestanias/noticias.jsx
@@ -1,85 +1,146 @@
 import './estilos/noticias.css'
 import lilix from '../assets/integrados/lilix.webp'
+import tpd from '../assets/integrados/tpd.png'
 import img1 from '../assets/actomos.png'
+
+import Pa from './noticias/personaje-a.jsx'
 
 import facebookLogo from '../assets/facebook.png'
 import instagramLogo from '../assets/instagram.png'
-import twitterLogo from '../assets/donarx.png'
+import xLogo from '../assets/donarx.png'
+
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom'
 
 const Noticias = () => {
-
-    const tarjetas = [
-          {
-            titular: 'Lilix de The piggy draws',
-            subtitulo: 'Un personaje que la marca The piggy draws nos ha brindado para incluir en otvers.',
-            noticia: 'Lilix es una Polymyr...',
-            image: lilix,
-            autor: 'The piggy draws',
-            autorImg: 'https://www.facebook.com/photo/?fbid=122145912518836879&set=a.122093148932836879',
-            redes: [
-              { url: 'https://www.facebook.com/profile.php?id=61575106370554', icon: facebookLogo },
-              { url: 'https://www.instagram.com/the.piggy.draws', icon: instagramLogo },
-              { url: 'https://twitter.com/thepiggydraws', icon: twitterLogo },
-            ],
-            urlv: ['#'], // agrega para que no rompa la lógica
-          },
-          {
-            titular:'TITULO',
-            subtitulo:'Subtítulo',
-            noticia:'NOTICIA',
-            image: img1, // corregido
-            autor:'-Autor',
-            urlv:['#'],
-          },
-          {
-            titular:'TITULO',
-            subtitulo:'Subtítulo',
-            noticia:'NOTICIA',
-            image:[img1],
-            autor:'-Autor',
-            urlv:['#'],
+  const tarjetas = [
+    {
+      titular: 'Lilix de Polymyr',
+      subtitulo: 'Personaje de la marca The piggy draws.',
+      noticia:
+        'Se ha llegado a un acuerdo con la marca para incluir a Lilix en otvers. Una extraterrestre de la raza Polymyr, de composición corporal libre del componente mineral óseo, siendo una masa libre y fácilmente moldeable; también cambia o modifica a voluntad la melamina de su cuerpo.',
+      image: lilix,
+      autorImg: tpd,
+      redes: [
+        {
+          href: 'https://www.facebook.com/profile.php?id=61575106370554',
+          logo: facebookLogo,
+          alt: 'Facebook',
         },
-    ]
+        {
+          href: 'https://www.instagram.com/the.piggy.draws',
+          logo: instagramLogo,
+          alt: 'Instagram',
+        },
+        {
+          href: 'https://twitter.com/thepiggydraws',
+          logo: xLogo,
+          alt: 'X/Twitter',
+        },
+      ],
+      urlv: ['#'],
+    },
+    {
+      titular: 'TITULO',
+      subtitulo: 'Subtítulo',
+      noticia: 'NOTICIA ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ...',
+      image: img1,
+      autorImg: null,
+      redes: [],
+      urlv: ['#'],
+    },
+    {
+      titular: 'TITULO',
+      subtitulo: 'Subtítulo',
+      noticia: 'NOTICIA ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ...',
+      image: img1,
+      autorImg: null,
+      redes: [],
+      urlv: ['#'],
+    },
+  ]
 
-    const mantener = (e) => {
-        e.preventDefault();
-    }
-    return (
+  const location = useLocation()
+  const esRutapa = location.pathname.endsWith('/Pa')
+
+  const mantener = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      <Routes>
+        <Route path="Pa" element={<Pa />} />
+      </Routes>
+
+      {!esRutapa && (
         <section className="Noticias">
+          <div className="cartas">
+            {tarjetas.map((card, index) => {
+              const noHayEnlace = card.urlv[0] === '#'
+              const almoadilla = noHayEnlace
+                ? { onClick: mantener, style: { cursor: 'default' } }
+                : { target: '_blank', href: card.urlv[0] }
 
-            <div className="cartas">
-
-                {
-                    tarjetas.map((card,index) => {
-                        const noHayEnlace = card.urlv[0] === '#';
-                        const almoadilla = noHayEnlace ? { onClick: mantener, style: { cursor: 'default' } } : { target: "_blank", href: card.urlv[0] };
-
-                        return(
-                            <div key={index} className="carta">
-
-                                <article className="titular">
-                                    <h1>{card.titular}</h1>
-                                    <h2 >{card.subtitulo}</h2>
-                                </article>
-                                <a {...almoadilla}>
-                                    <div className="noticia">
-                                            <article className="articulo">
-                                                 <h2 className="noticia">{card.noticia}</h2>
-                                                 <p className="autor">
-                                                    <img src={card.autorImg} alt={card.autor} />
-                                                 </p>
-                                            </article>
-                                            <img src={card.image} />
-                                    </div>
-                                </a>
-                            </div>
+              return (
+                <div key={index} className="carta">
+                  <article className="titular">
+                    <h1>{card.titular}</h1>
+                    <h2>{card.subtitulo}</h2>
+                  </article>
+                  <div
+                    onClick={() => {
+                      if (card.urlv[0] !== '#') {
+                        window.open(
+                          card.urlv[0],
+                          '_blank',
+                          'noopener,noreferrer'
                         )
-                    })
-                }
-
-            </div>
+                      }
+                    }}
+                    style={{
+                      cursor: card.urlv[0] !== '#' ? 'pointer' : 'default',
+                    }}
+                  >
+                    <div className="noticia">
+                      <img src={card.image} alt={card.titular} />
+                      <article className="articulo">
+                        <h2>{card.noticia}</h2>
+                        {card.autorImg && (
+                          <p className="autor">
+                            <img
+                              className="autorImg"
+                              src={card.autorImg}
+                              alt="Autor"
+                            />
+                          </p>
+                        )}
+                        <div className="redes">
+                          {card.redes.map((red, i) => (
+                            <a
+                              key={i}
+                              href={red.href}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <img
+                                src={red.logo}
+                                alt={red.alt}
+                                className="redImg"
+                              />
+                            </a>
+                          ))}
+                        </div>
+                      </article>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
         </section>
-    )
+      )}
+    </>
+  )
 }
 
-export default Noticias;
+export default Noticias
---
+++ src/menu/pestanias/noticias/personaje-a.jsx
@@ -0,0 +1,41 @@
+import '../estilos/noticias.css'
+import lilix from '../../assets/integrados/lilix.webp'
+import { useEffect, useState } from 'react'
+
+const Thepiggydraws = () => {
+  // Texto completo que quieres mostrar
+  const textoCompleto = `Lilix de Polymyr. Personaje de la marca The piggy draws. 
+    Se ha llegado a un acuerdo con la marca para incluir a Lilix en otvers. 
+    Una extraterrestre de la raza Polymyr, de composición corporal libre del componente mineral óseo, 
+    siendo una masa libre y fácilmente moldeable; también cambia o modifica a voluntad la melamina 
+    de su cuerpo.`
+
+  // Estado para el efecto de "escribir"
+  const [texto, setTexto] = useState("")
+
+  useEffect(() => {
+    let i = 0
+    const intervalo = setInterval(() => {
+      setTexto(textoCompleto.slice(0, i))
+      i++
+      if (i > textoCompleto.length) clearInterval(intervalo)
+    }, 50) // velocidad de escritura (ms)
+    return () => clearInterval(intervalo)
+  }, [])
+
+  return (
+    <section className="Noticias">
+      <div className="blog-container">
+        {/* Imagen en la esquina superior izquierda */}
+        <img src={lilix} alt="Lilix de Polymyr" className="blog-img" />
+
+        {/* Texto con efecto de escritura */}
+        <article className="blog-text">
+          <p>{texto}</p>
+        </article>
+      </div>
+    </section>
+  )
+}
+
+export default Thepiggydraws
